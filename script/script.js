38648d9127be79c7dbceffbd8ae7c4e670855018
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-10-05 19:06:08 +0530

    Adding lightbox...

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
 // JS Scripts
+const lightBox = document.createElement("div");
+lightBox.id = "lightbox";
+document.body.appendChild(lightBox);
+
+const images = document.querySelectorAll(".card-image");
+images.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    lightBox.classList.add("active");
+    const img = document.createElement("img");
+    while (lightBox.firstChild) {
+      lightBox.removeChild(lightBox.firstChild);
+    }
+    img.src = image.src;
+    img.id = "lightbox-image";
+    lightBox.appendChild(img);
+  });
+});
+
+lightBox.addEventListener("click", (e) => {
+  if (e.target.tagName !== "IMG") {
+    lightBox.classList.remove("active");
+  }
+});
